15b1a244c0cfd3b2dd1abb50fdbf69fcb9730d10
hacdroid <58026431+hacdroid@users.noreply.github.com>
2019-11-26 13:29:11 +0100

    Update Chair.java

--- src/themanor/Chair.java
+++ src/themanor/Chair.java
@@ -19,4 +19,4 @@ public class Chair extends Item {
     public String toString() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}
+}//hey
